39b6c92ee1a4fd257820f01f379e1bc2dcf8af2a
ashleyglenn0 <65840934+ashleyglenn0@users.noreply.github.com>
2021-05-07 22:07:08 -0500

    changed button color

--- facerecognition/src/components/ImageLinkForm/ImageLinkForm.js
+++ facerecognition/src/components/ImageLinkForm/ImageLinkForm.js
@@ -8,7 +8,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
             <div className='center'>
                 <div className=' form center pa4 br3 shadow-5'>
                     <input className='f4 pa2 w-70 center' type='text' onChange={ onInputChange }></input>
-                    <button className='w-30 grow f4 link ph3 pv2 dib white bg-dark-green'
+                    <button className='w-30 grow f4 link ph3 pv2 dib white bg-dark-blue'
                     onClick={ onButtonSubmit }>Detect</button>
                 </div>
             </div>
